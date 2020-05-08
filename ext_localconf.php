a873073ca5da31fc9e13d37eb7e9355fe6400796
TYPO3 Integrator <vasyl@typo3.net.ua>
2020-05-08 17:45:53 +0300

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,11 +2,47 @@
 defined('TYPO3_MODE') || die();
 
 /***************
- * Add default RTE configuration
+ * Make the extension configuration accessible
  */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['forms_package'] = 'EXT:forms_package/Configuration/RTE/Default.yaml';
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+);
+$bootstrapPackageConfiguration = $extensionConfiguration->get('forms_package');
 
 /***************
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:forms_package/Configuration/TsConfig/Page/All.tsconfig">');
+
+/***************
+ * Register custom EXT:form configuration
+ */
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('form')) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
+        module.tx_form {
+            settings {
+                yamlConfigurations {
+                    110 = EXT:forms_package/Configuration/Form/Setup.yaml
+                }
+            }
+        }
+        plugin.tx_form {
+            settings {
+                yamlConfigurations {
+                    110 = EXT:forms_package/Configuration/Form/Setup.yaml
+                }
+            }
+        }
+    '));
+}
+
+/***************
+ * Add default RTE configuration
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['forms_package'] = 'EXT:forms_package/Configuration/RTE/Default.yaml';
+
+/***************
+ * Register "t3ua" as global fluid namespace
+ */
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['t3ua'][] = 'T3UA\\FormsPackage\\ViewHelpers';
+
